17c164b5b0e8260fb9540d629107261424021705
DESKTOP-LPF0K0B\59858 <598586050@qq.com>
2024-11-04 11:50:21 +0800

    feature:【阶段提交】

--- mysql.go
+++ mysql.go
@@ -24,7 +24,7 @@ type Source struct {
 }
 
 type tdb struct {
-	link      string
+	instance  *sqlx.DB
 	tableName string
 	whereStr  string
 	fieldStr  string
@@ -37,7 +37,8 @@ type begin struct {
 	source []Source
 }
 
-func createInstance(source ...Source) *Source {
+// 创建连接池
+func createInstance(source ...Source) *sqlx.DB {
 	config := &Source{
 		Link:        tgcfg.Config.GetMySqlSource("default.link").String(),
 		MaxOpen:     int(tgcfg.Config.GetMySqlSource("default.maxOpen").Int()),
@@ -54,8 +55,8 @@ func createInstance(source ...Source) *Source {
 			MaxLifeTime: source[0].MaxLifeTime,
 		}
 	}
-	if _, ok := dbInstance.Load(config.Link); ok {
-		return config
+	if ins, ok := dbInstance.Load(config.Link); ok {
+		return ins.(*sqlx.DB)
 	}
 	instance, err := sqlx.Connect("mysql", config.Link)
 	if err != nil {
@@ -71,138 +72,146 @@ func createInstance(source ...Source) *Source {
 	instance.SetConnMaxIdleTime(time.Duration(config.MaxIdleTime) * time.Second)
 	instance.SetConnMaxLifetime(time.Duration(config.MaxLifeTime) * time.Second)
 	dbInstance.Store(config.Link, instance)
-	return config
+	return instance
 }
 
-func Begin(source ...Source) *begin {
-	config := createInstance(source...)
-	var t *sqlx.Tx
-	if instance, ok := dbInstance.Load(config.Link); ok {
-		tx, err := instance.(*sqlx.DB).Beginx()
-		if err != nil {
-			t = tx
-		} else {
-			panic(Exception{
-				StateCode: http.StatusInternalServerError,
-				ErrorCode: ErrorCode.EXCEPTION,
-				Message:   "执行Begin出错",
-				Error:     err,
-			})
-		}
+// BeginTransaction 开启事务,如果不传数据源默认走的是配置文件里默认的,传了可以指定任意的数据源
+func BeginTransaction(source ...Source) *begin {
+	instance := createInstance(source...)
+	tx, err := instance.Beginx()
+	if err != nil {
+		panic(Exception{
+			StateCode: http.StatusInternalServerError,
+			ErrorCode: ErrorCode.EXCEPTION,
+			Message:   "执行Begin出错",
+			Error:     err,
+		})
 	}
 	return &begin{
-		tx:     t,
+		tx:     tx,
 		source: source,
 	}
 }
 
+// Db 事务去操作数据库
 func (b *begin) Db(tableName string) *tdb {
-	return Db(tableName, b.source...)
+	db := Db(tableName, b.source...)
+	db.tx = b.tx
+	return db
 }
 
+// Commit 提交事务
 func (b *begin) Commit() error {
 	return b.tx.Commit()
 }
 
+// Rollback 事务回滚
 func (b *begin) Rollback() error {
 	return b.tx.Rollback()
 }
 
 // Db 如果不传数据源默认走的是配置文件里默认的,传了可以指定任意的数据源
 func Db(tableName string, source ...Source) (db *tdb) {
-	config := createInstance(source...)
+	instance := createInstance(source...)
 	return &tdb{
-		link:      config.Link,
+		instance:  instance,
 		tableName: tableName,
 		whereStr:  "",
 		fieldStr:  "*",
 	}
 }
 
+// Field 指定查询的字段,默认不去重
 func (db *tdb) Field(fields string, distinct ...bool) *tdb {
-	n := *db
 	dis := ""
 	if len(distinct) > 0 && distinct[0] {
 		dis = "DISTINCT "
 	}
-	n.fieldStr = fmt.Sprintf("%s%s", dis, fields)
-	return &n
+	db.fieldStr = fmt.Sprintf("%s%s", dis, fields)
+	return db
 }
 
+// Where 指定查询条件, tg.Db().Where("id", "=", 1)
 func (db *tdb) Where(field string, condition string, value interface{}) *tdb {
-	n := *db
-	n.whereStr += fmt.Sprintf("WHERE %s %s ?", field, condition)
-	n.values = append(n.values, value)
-	return &n
+	db.whereStr += fmt.Sprintf("WHERE %s %s ?", field, condition)
+	db.values = append(db.values, value)
+	return db
 }
 
+// WhereAnd 指定多查询条件,前面必须有Where, tg.Db().Where("age", "=", 18).WhereAnd("gender", "=", 1)
 func (db *tdb) WhereAnd(field string, condition string, value interface{}) *tdb {
-	n := *db
-	n.whereStr += fmt.Sprintf(" AND %s %s ?", field, condition)
-	n.values = append(n.values, value)
-	return &n
+	db.whereStr += fmt.Sprintf(" AND %s %s ?", field, condition)
+	db.values = append(db.values, value)
+	return db
 }
 
+// WhereOr 指定多查询条件,前面必须有Where, tg.Db().Where("age", "=", 18).WhereOr("age", "=", 19)
 func (db *tdb) WhereOr(field string, condition string, value interface{}) *tdb {
-	n := *db
-	n.whereStr += fmt.Sprintf(" OR %s %s ?", field, condition)
-	n.values = append(n.values, value)
-	return &n
+	db.whereStr += fmt.Sprintf(" OR %s %s ?", field, condition)
+	db.values = append(db.values, value)
+	return db
 }
 
+// FindOne 查询一条数据
 func (db *tdb) FindOne(scan any) error {
-	n := *db
-	if instance, ok := dbInstance.Load(n.link); ok {
-		query := fmt.Sprintf("SELECT %s FROM %s %s LIMIT 1", n.fieldStr, n.tableName, n.whereStr)
-		stmt, err := instance.(*sqlx.DB).Preparex(query)
-		if err != nil {
-			return err
-		}
-		defer stmt.Close()
+	query := fmt.Sprintf("SELECT %s FROM %s %s LIMIT 1", db.fieldStr, db.tableName, db.whereStr)
+	var stmt *sqlx.Stmt
+	var err error
+	if db.tx != nil {
+		stmt, err = db.tx.Preparex(query)
+	} else {
+		stmt, err = db.instance.Preparex(query)
+	}
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
 
-		v := reflect.ValueOf(scan)
-		if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Struct {
-			panic(Exception{
-				StateCode: http.StatusInternalServerError,
-				ErrorCode: ErrorCode.MySqlError,
-				Message:   "必须是指向结构体的指针",
-			})
-		}
+	v := reflect.ValueOf(scan)
+	if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Struct {
+		panic(Exception{
+			StateCode: http.StatusInternalServerError,
+			ErrorCode: ErrorCode.MySqlError,
+			Message:   "必须是指向结构体的指针",
+		})
+	}
 
-		udb := stmt.Unsafe()
-		err = udb.Get(scan, n.values...)
-		if err != nil {
-			return err
-		}
-
+	udb := stmt.Unsafe()
+	err = udb.Get(scan, db.values...)
+	if err != nil {
+		return err
 	}
 	return nil
 }
 
+// Select 查询多条数据
 func (db *tdb) Select(scan any) error {
-	n := *db
-	if instance, ok := dbInstance.Load(n.link); ok {
-		query := fmt.Sprintf("SELECT %s FROM %s %s", n.fieldStr, n.tableName, n.whereStr)
-		stmt, err := instance.(*sqlx.DB).Preparex(query)
-		if err != nil {
-			return err
-		}
-		defer stmt.Close()
+	query := fmt.Sprintf("SELECT %s FROM %s %s", db.fieldStr, db.tableName, db.whereStr)
+	var stmt *sqlx.Stmt
+	var err error
+	if db.tx != nil {
+		stmt, err = db.tx.Preparex(query)
+	} else {
+		stmt, err = db.instance.Preparex(query)
+	}
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
 
-		v := reflect.ValueOf(scan)
-		if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Slice {
-			panic(Exception{
-				StateCode: http.StatusInternalServerError,
-				ErrorCode: ErrorCode.MySqlError,
-				Message:   "必须是指向切片的指针",
-			})
-		}
+	v := reflect.ValueOf(scan)
+	if v.Kind() != reflect.Ptr || v.Elem().Kind() != reflect.Slice {
+		panic(Exception{
+			StateCode: http.StatusInternalServerError,
+			ErrorCode: ErrorCode.MySqlError,
+			Message:   "必须是指向切片的指针",
+		})
+	}
 
-		udb := stmt.Unsafe()
-		err = udb.Select(scan, n.values...)
-		if err != nil {
-			return err
-		}
+	udb := stmt.Unsafe()
+	err = udb.Select(scan, db.values...)
+	if err != nil {
+		return err
 	}
 	return nil
 }
